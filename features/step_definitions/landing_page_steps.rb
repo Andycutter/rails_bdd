8231fc90513da31edb0f6f8c3102032ce0ca23c3
AndreasMN <andycutter@godmail.dk>
2018-07-03 21:13:38 +0200

    add step definition

--- features/step_definitions/landing_page_steps.rb
+++ features/step_definitions/landing_page_steps.rb
@@ -31,3 +31,7 @@ Given("I visit the {string} page") do |string|
   Then("I should be on {string} page") do |string|
     pending # Write code here that turns the phrase above into concrete actions
   end
+
+  Then("I should not see {string} with {string}") do |string, string2|
+    pending # Write code here that turns the phrase above into concrete actions
+  end

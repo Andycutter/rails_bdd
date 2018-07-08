555d17e21a3e239c6c74f311113f2fbf37151dcb
AndreasMN <andycutter@godmail.dk>
2018-07-08 23:39:59 +0200

    add made assiciationen of article to many comments made rspec and cucumber go green

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,8 @@
 class CommentsController < ApplicationController
   def create
     article = Article.find(params[:article_id])
-    Comment.create(email: params[:email], content: params[:comment], article: article)  
+    Comment.create(email: params[:email], content: params[:comment], article: article)
+    flash[:succes] = 'Your comment was added'
     redirect_to article_path(article)
   end
 end

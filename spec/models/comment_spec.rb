b9dff96e8fb36009f5de89c3fd2d8b9b8b5f0652
AndreasMN <andycutter@godmail.dk>
2018-07-08 23:07:53 +0200

    add rspec test of "assocations" of comment to article. test go green

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -7,4 +7,8 @@ RSpec.describe Comment, type: :model do
     it { is_expected.to have_db_column :email } 
     it { is_expected.to have_db_column :content }
   end
+
+  describe 'Associations' do
+    it { is_expected.to belong_to :article }
+  end
 end

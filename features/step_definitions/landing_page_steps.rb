fc1b08d53d1026e0a23bac1b5b5affddc3f6b4dc
AndreasMN <andycutter@godmail.dk>
2018-07-08 10:18:31 +0200

    add text and "show me the page" to better follow flow on page

--- features/step_definitions/landing_page_steps.rb
+++ features/step_definitions/landing_page_steps.rb
@@ -1,39 +1,43 @@
+Then ('Show me the page') do
+  save_and_open_page
+end
+
 When ('I visit the site') do
-    visit root_path
+  visit root_path
 end
 
 Then ('I should see {string}') do |content|
-    expect(page).to have_content content
+  expect(page).to have_content content
 end
 
 Given ("the following articles exists") do |table|
-    table.hashes.each do |article|
-        Article.create!(article)
-    end
+  table.hashes.each do |article|
+      Article.create!(article)
+  end
 end
 
 Given("I visit the {string} page") do |string|
-    visit root_path
-  end
+  visit root_path
+end
   
-  When("I click {string} link") do |article_link|
-    click_link(article_link)
-  end
+When("I click {string} link") do |article_link|
+  click_link(article_link)
+end
   
-  When("I fill in {string} with {string}") do |field, content|
-    fill_in field, with: content
-  end
-  
-  When("I click {string} button") do |submit|
-    click_button('Create Article')
-  end
-  
-  Then("I should be on {string} page") do |string|
-    visit create_article_path
-    #I want to check if I go to the new create page with the new article
-  end
+When("I fill in {string} with {string}") do |field, content|
+  fill_in field, with: content
+end
 
-  Then("I should not see {string} with {string}") do |new_title, new_content|
-    expect(page).to have_content new_title, new_content
-    #I want to see if the title and content I added in the form is displayed on the new article site
-  end
+When("I click {string} button") do |submit|
+  click_button('Create Article')
+end
+
+Then("I should be on {string} page") do |string|
+  visit create_article_path
+  #I want to check if I go to the new create page with the new article
+end
+
+Then("I should not see {string} with {string}") do |new_title, new_content|
+  expect(page).to have_content new_title, new_content
+  #I want to see if the title and content I added in the form is displayed on the new article site
+end

e14e8c7035456e4f664dd63e0ceb09b00efd81aa
AndreasMN <andycutter@godmail.dk>
2018-07-13 14:51:32 +0200

    refactored validations in comment.rb

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,5 @@ class Comment < ApplicationRecord
   belongs_to :article
 
   validates_presence_of :content
-  validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, message: 'Invalid Email'
+  validates_confirmation_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create
 end

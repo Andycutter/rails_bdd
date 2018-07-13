1cf3df8a6d1ebf5b919634378ec4e63367089cc2
AndreasMN <andycutter@godmail.dk>
2018-07-13 15:12:22 +0200

    refactored validations in article.rb

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,4 @@
 class Article < ApplicationRecord
     has_many :comments
-    validates :title, presence: true
-    validates :content, presence: true
-    validates :author, presence: true
+    validates_presence_of :title, :content, :author
 end
